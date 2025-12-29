da2d0ee5d3d6a3e2fc32a2f041bace5faad57984
Adrián Gordillo <adriangordillo96@gmail.com>
2025-12-29 16:42:26 +0100

    Final Commit

---
+++ EchoClient.java
@@ -0,0 +1,39 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class EchoClient {
+    public static void main(String[] args) {
+
+        try {
+            System.out.println("Conecting to server...");
+            Socket socket = new Socket("127.0.0.1", 5000);
+            System.out.println("Successfully Connected!");
+
+            InputStreamReader inputStreamReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader reader = new BufferedReader(inputStreamReader);
+            PrintWriter writer = new PrintWriter(socket.getOutputStream(), true);
+            Scanner scanner = new Scanner(System.in);
+
+            BufferedReader stdReader = new BufferedReader(new InputStreamReader(System.in));
+
+            String userInput;
+            System.out.println("Type message:");
+            while ((userInput = stdReader.readLine()) != null) {
+                writer.println(userInput);
+                System.out.println("Server:" + reader.readLine());
+            }
+
+            socket.close();
+            scanner.close();
+            reader.close();
+            writer.close();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}
